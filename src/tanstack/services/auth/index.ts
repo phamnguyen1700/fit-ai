39620ecf90da3f00850b841853f8b11c0e47eac1
Thảo Vi <146309437+ThDziii@users.noreply.github.com>
2025-11-03 22:44:33 +0700

    update env

--- src/tanstack/services/auth/index.ts
+++ src/tanstack/services/auth/index.ts
@@ -2,4 +2,4 @@ import { post } from '@/shared/api/http'
 import { ILoginRequest, ILoginResponse } from '@/types/auth'
 
 // Service login
-export const loginService = (data: ILoginRequest) => post<ILoginResponse>('/auth/admin/login', data)
+export const loginService = (data: ILoginRequest) => post<ILoginResponse>('account/api/auth/admin/login', data)
--- src/tanstack/services/users/index.ts
+++ src/tanstack/services/users/index.ts
@@ -1,10 +1,10 @@
 import { post, get, put } from '@/shared/api/http'
 import { IRegister, Params, UserState, UserDetail } from '@/types/users'
 
-export const registerService = (data: IRegister) => post('/auth/register', data)
+export const registerService = (data: IRegister) => post(`account/api/auth/register`, data)
 
-export const getUsersService = (params: Params) => get<UserState>(`/user`, { params })
+export const getUsersService = (params: Params) => get<UserState>(`account/api/user`, { params })
 
-export const updateUserStatusService = (id: string) => put(`/user/${id}/update-status`)
+export const updateUserStatusService = (id: string) => put(`account/api/user/${id}/update-status`)
 
-export const getUserDetail = (id: string) => get<UserDetail>(`/user/${id}/detail`)
+export const getUserDetail = (id: string) => get<UserDetail>(`account/api/user/${id}/detail`)
